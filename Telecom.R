468c671d30d00300e7f7a1fa6150ad24d2d246c5
ManuelKay1 <kwameacquah294@gmail.com>
2026-04-01 05:39:26 +1200

    Update

--- Telecom.R
+++ Telecom.R
@@ -56,3 +56,37 @@ Yes_churn <- telco %>% filter(churn_label == 'Yes')
 table(telco$churn_value)
 
 table(telco$churn_label)
+
+ggplot(data=telco, mapping = aes(x= contract, fill = churn_label)) + geom_bar(position = 'fill')
+# from the bar chart created we find out that customers who are on a Month-to-Month contract are the ones the company is most likely to loose
+
+ggplot(data=telco, mapping = aes(x = monthly_charges, fill = churn_label)) + geom_density(alpha = 0.5) + labs(title = 'Churn By Monthly Charges')
+# the question we are trying to ask here is do customers with high charges churn
+
+colnames(telco)
+
+# check if the tenure_month affects the churn
+ggplot(data = telco, mapping = aes(x = tenure_months, fill = churn_label)) + geom_histogram(bins = 30, alpha = 0.6) + labs(title = "Churn By Tenure")
+# the graph is telling us that the more the tenure increases the customers are likely to stay
+
+ggplot(data = telco, mapping = aes(x = payment_method, fill = churn_label)) + geom_bar(position = 'fill') + labs(title = 'payment method by churn')
+
+
+# build a model, The Goal is will a customer churn or not 
+
+telco <- telco %>% select(-customer_id)
+
+telco$churn_label <- as.factor(telco$churn_label)
+
+# Train or Test Split
+
+set.seed(123)
+
+train_index <- sample(nrow(telco), 0.7 * nrow(telco))
+
+train_data <- telco[train_index,]
+test_data <- telco[-train_index,] 
+
+model <- glm(churn_label~., data = train_data, family = 'binomial')
+
+sapply(train_data, function(x) length(unique(x)))
